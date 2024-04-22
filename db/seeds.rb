d1d58545c447535d1d3d5385b548d94ee2725d93
Aidan-Johnson <asjohnsonstu@gmail.com>
2024-04-22 02:04:32 -0500

    added product model,controller,index and show pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Product.create(
+  name: "Water Bottle",
+  price: 25.00,
+  description: "A 24oz stainless steel water bottle"
+
+)
+
+Product.create(
+  name: "Hiking Back Pack",
+  price: 30.00,
+  description: "A water proof light weight hiking backpack"
+
+)
+
+Product.create(
+  name: "Tent",
+  price: 45.00,
+  description: "Outdoor dome camping tent that holds 4 persons"
+
+)
